ccd9fb99cfd0e1355aaf306c132e7cbd2152d39f
David Lewis and Roger Yee <roger@>
2018-01-08 15:37:58 -0800

    Add letter rearrangement and capitalization

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var str1 = prompt("Please type a sentance: ");
+/*var str1 = prompt("Please type a sentance: ");
 
 alert(str1);
 
@@ -20,3 +20,62 @@ alert(strLength);
 var partStr = str1.substr(1, strLength);
 
 alert(partStr);
+*/
+
+var sentence = prompt("Please type a sentence: ");
+
+var partStr = function(str1, strLength, prtStr) {
+  strLength = (str1.length);
+  strLength -= 2;
+  prtStr = str1.substr(1, strLength);
+  return(prtStr);
+};
+
+var middleStr = partStr(sentence);
+alert(middleStr);
+
+/*
+var capitol = function(str1, firstLetter, lastLetter, prtStr, strLength){
+  var firstLetter = str1.charAt(0);
+  firstLetter = firstLetter.toUpperCase();
+  var lastLetter = str1.slice(-1);
+  lastLetter = lastLetter.toUpperCase();
+  strLength = (str1.length);
+  strLength -= 2;
+  prtStr = str1.substr(1, strLength);
+  alert(firstLetter + prtStr + lastLetter);
+};
+
+capitol(sentence);
+
+var arrange = function(str1, firstLetter, lastLetter, strLength, prtStr) {
+
+var firstLetter = str1.charAt(0);
+  alert(firstLetter.toUpperCase());
+var lastLetter = str1.slice(-1);
+  alert(lastLetter.toUpperCase());
+var strLength = (str1.length);
+  alert(strLength -= 2);
+var prtStr = str1.substr(1, strLength);
+
+alert(lastLetter + prtStr + firstLetter);
+};
+
+arrange(sentence);
+*/
+
+var letter1 = function(str1, firstLetter) {
+  var firstLetter = str1.charAt(0);
+  firstLetter = firstLetter.toUpperCase();
+  return(firstLetter);
+};
+alert(letter1(sentence));
+
+var letter2 = function(str1, lastLetter) {
+  var lastLetter = str1.slice(-1);
+  lastLetter = lastLetter.toUpperCase();
+  return(lastLetter);
+};
+
+alert(letter2(sentence) + letter1(sentence));
+alert(letter1(sentence) + letter2(sentence));
